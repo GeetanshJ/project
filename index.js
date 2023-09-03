6363e8d2e1dac8108c93718c9ffc06e0a634ee3c
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2023-09-03 16:39:40 +0530

    updated

---
+++ index.js
@@ -0,0 +1,6 @@
+const main_header_part_1 = document.querySelector(".listing");
+
+document.querySelector(".nav-bar-ham").addEventListener("click",()=>{
+        main_header_part_1.classList.toggle("active");
+})
+
